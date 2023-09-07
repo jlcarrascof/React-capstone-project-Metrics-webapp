988df39e65ffb30faa5ff71e874f494733134a21
Javier Martinez <entrenamientospuntonet@gmail.com>
2023-09-07 19:04:07 -0400

    Update index.js from previous project

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import App from './App';
-import './assets/styles.css';
-import store from './redux/store';
+import { RouterProvider } from 'react-router-dom';
+import router from './router/router';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
+  <>
+    <RouterProvider router={router} />
+  </>,
 );
