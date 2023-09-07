e9d5e24f3a7c467206cd2ae69a6deedad255d163
Javier Martinez <entrenamientospuntonet@gmail.com>
2023-09-07 18:57:07 -0400

    Update App.js from previous project

--- src/App.js
+++ src/App.js
@@ -1,21 +1,10 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Rockets from './components/Rockets';
-import NavBar from './components/NavBar';
-import Missions from './components/Missions';
-import MyProfile from './components/MyProfile';
+import './assests/styles.css';
 
 function App() {
   return (
-    <div>
-      <BrowserRouter>
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<Rockets />} />
-          <Route path="/missions" element={<Missions />} />
-          <Route path="/my-profile" element={<MyProfile />} />
-        </Routes>
-      </BrowserRouter>
+    <div className="App">
+      <h1>Hello React Capstone</h1>
+      <p>Javier J. Martinez F.</p>
     </div>
   );
 }
