3661c2db087f951c83e6ef72929ad4f4ca7a59c7
Javier Martinez <entrenamientospuntonet@gmail.com>
2023-09-07 23:01:05 -0400

    Update Countries and Details files

--- src/pages/Countries.js
+++ src/pages/Countries.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import CountriesList from '../components/CountriesList';
+import continent from '../assests/world-map.png';
 
-const countries = () => (
-  <div>
-    <p>Country</p>
-  </div>
+const Countries = () => (
+  <>
+    <img src={continent} alt="Continents in the world" className="home-bg" />
+    <div className="title-box flex">
+      <h1>Europe</h1>
+      <h2>Air Pollution Stats</h2>
+      <span>2023</span>
+    </div>
+    <CountriesList />
+  </>
 );
 
-export default countries;
+export default Countries;
